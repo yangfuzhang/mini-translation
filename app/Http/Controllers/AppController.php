3d8fd5fa7c6530fb101a1b2b19aa176dbf90d4ff
yangfuzhang <yangfuzhang0720@126.com>
2018-06-09 08:59:56 +0800

    project updated

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -36,7 +36,7 @@ class AppController extends Controller
 
         $http = new Client;
 
-        $response = $http->request('POST', 'https://recognition.image.myqcloud.com/ocr/general', [
+        $response = $http->request('GET', 'https://www.limepietech.com', [
                         'headers' => [
                             'Host'=>'recognition.image.myqcloud.com',
                             'Authorization'  => $signStr,
@@ -52,14 +52,30 @@ class AppController extends Controller
                         'timeout' => 5
                     ]);
 
-        $res = json_decode((string) $response->getBody(), true);
-        $results = $res['data']['items'];
-        $text = '';
+        // $response = $http->request('POST', 'https://recognition.image.myqcloud.com/ocr/general', [
+        //                 'headers' => [
+        //                     'Host'=>'recognition.image.myqcloud.com',
+        //                     'Authorization'  => $signStr,
+        //                     'Content-Length' => 187,
+        //                     'Content-Type'   => 'application/json'
+        //                 ],
+        //                 'json' => [
+        //                     'appid'  => $appid,
+        //                     'bucket' => $bucket,
+        //                     'url'    => 'http://limepie-1253144008.picgz.myqcloud.com/微信图片_20180609053635.jpg'
+        //                 ],
+        //                 'verify' => false,
+        //                 'timeout' => 5
+        //             ]);
+
+        // $res = json_decode((string) $response->getBody(), true);
+        // $results = $res['data']['items'];
+        // $text = '';
 
 
-        foreach($results as $result) {
-            $text .= $result['itemstring'].'<br>';
-        }
+        // foreach($results as $result) {
+        //     $text .= $result['itemstring'].'<br>';
+        // }
 
         return response()->json(['text'=>'success']);
     }
