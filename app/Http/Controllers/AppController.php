d45da1ee19de24b22b83e0bf5f94d0f8c4d3e54a
yangfuzhang <yangfuzhang0720@126.com>
2018-09-09 20:48:11 +0800

    project updated

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -17,7 +17,7 @@ class AppController extends Controller
     public function ffmpeg() {
         $ffmpeg = \FFMpeg\FFMpeg::create(array(
             'ffmpeg.binaries'  => '/usr/bin/ffmpeg',
-            'ffprobe.binaries' => '/usr/bin/ffmpeg',
+            'ffprobe.binaries' => '/usr/bin/ffprobe',
             'timeout'          => 3600, // The timeout for the underlying process
             'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
         ));
