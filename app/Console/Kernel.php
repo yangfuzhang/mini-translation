a8ee4584d76d8fb37772b9494e50e65304095c07
yangfuzhang <yangfuzhang0720@126.com>
2018-09-09 17:44:33 +0800

    project updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->exec('rm /var/www/html/public/images/upload/*')
+                 ->everyMinute();
     }
 
     /**
