21aa9697290bce33e77564e31dceaf612f8988d5
yangfuzhang <yangfuzhang0720@126.com>
2020-03-02 16:25:03 +0800

    project updated

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -41,7 +41,6 @@ class AppController extends Controller
     public function recognize(Request $request) {
         $path = $request->path;
         $rec_type = $request->rec_type;
-        $id_card_side = $request->id_card_side;
 
         $client_id = 'IGpdCaDx14qf8lWfWG00FHwc';
         $client_secret = 'pyeGLISkbeQyjUotB2bmHTtw5c8kqfqp';
@@ -68,10 +67,6 @@ class AppController extends Controller
 
         switch($rec_type) {
             //通用文字识别
-            case 'print':
-                $api_url =  'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic';
-                $results = $this->requestGeneralApi($api_url, $access_token, $path);
-                break;
             case 'general':
                 $api_url =  'https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic';
                 $results = $this->requestGeneralApi($api_url, $access_token, $path);
@@ -80,16 +75,6 @@ class AppController extends Controller
             case 'webimage':
                 $api_url =  'https://aip.baidubce.com/rest/2.0/ocr/v1/webimage';
                 $results = $this->requestGeneralApi($api_url, $access_token, $path);
-                break;
-            //身份证识别
-            case 'idcard':
-                $api_url =  'https://aip.baidubce.com/rest/2.0/ocr/v1/idcard';
-                $results = $this->requestIdcardApi($api_url, $access_token, $path, $id_card_side);
-                break;
-            //银行卡识别
-            case 'bankcard':
-                $api_url =  'https://aip.baidubce.com/rest/2.0/ocr/v1/bankcard';
-                $results = $this->requestBankcardApi($api_url, $access_token, $path);
                 break;
             default:
                 break;
@@ -134,106 +119,6 @@ class AppController extends Controller
         } else {
             return false;
         } 
-    }
-
-    public function requestIdcardApi($api_url, $access_token, $path, $id_card_side) {
-        $http = new Client;
-        //$image = file_get_contents("https://www.limepietech.com/public/images/upload/GyMH265WqptKQNNcjum0KdOTqHBddIeFPLxMV8u1.jpeg");
-        $image = file_get_contents($path);
-        $image = base64_encode($image);
-
-        $response = $http->request('POST', $api_url, [
-                        'headers' => [
-                            'Content-Type'   => 'application/x-www-form-urlencoded'
-                        ],
-                        'form_params' => [
-                            'access_token'=>$access_token,
-                            'detect_direction' => 'true',
-                            'image' => $image,
-                            'id_card_side' => $id_card_side
-                        ],
-                        'verify' => false
-                    ]);
-
-        $res = json_decode((string) $response->getBody(), true);
-        
-
-        if(!isset($res['error_code']))  {
-            //正面和反面返回的数据不一样
-            $words_result = $res['words_result'];
-            $results = [];
-
-            if($id_card_side === 'front') {
-                $results['姓名'] = isset($words_result['姓名']) ? $words_result['姓名']['words']:'无法识别';
-                $results['性别'] = isset($words_result['性别']) ? $words_result['性别']['words']:'无法识别';
-                $results['民族'] = isset($words_result['民族']) ? $words_result['民族']['words']:'无法识别';
-                $results['出生'] = isset($words_result['出生']) ? $words_result['出生']['words']:'无法识别';
-                $results['住址'] = isset($words_result['住址']) ? $words_result['住址']['words']:'未识别';
-                $results['公民身份号码'] = isset($words_result['公民身份号码']) ? $words_result['公民身份号码']['words']:'无法识别';
-
-                return $results;
-            }
-
-            $results['签发机关'] = isset($words_result['签发机关']) ? $words_result['签发机关']['words']:'无法识别';
-            $results['签发日期'] = isset($words_result['签发日期']) ? $words_result['签发日期']['words']:'无法识别';
-            $results['失效日期'] = isset($words_result['失效日期']) ? $words_result['失效日期']['words']:'无法识别';
-
-            return $results;
-            
-        } else {
-            return false;
-        }
-    }
-
-    public function requestBankcardApi($api_url, $access_token, $path) {
-        $http = new Client;
-        //$image = file_get_contents("https://www.limepietech.com/public/images/upload/Wyu2gHzyrsObSEo1BXlK4tJNdEIgW8ZO9y6Bblaw.png");
-        $image = file_get_contents($path);
-        $image = base64_encode($image);
-
-        $response = $http->request('POST', $api_url, [
-                        'headers' => [
-                            'Content-Type'   => 'application/x-www-form-urlencoded'
-                        ],
-                        'form_params' => [
-                            'access_token'=>$access_token,
-                            'detect_direction' => 'true',
-                            'image' => $image
-                        ],
-                        'verify' => false
-                    ]);
-
-        $res = json_decode((string) $response->getBody(), true);
-        
-
-        if(!isset($res['error_code']))  {
-            $results = $res['result'];
-            $result = [];
-
-            $result['bank_card_number'] = isset($results['bank_card_number']) ? $results['bank_card_number'] : '无法识别';
-            $result['bank_name'] = isset($results['bank_name']) ? $results['bank_name'] : '无法识别';
-            if(isset($results['bank_card_type'])) {
-                switch($results['bank_card_type']) {
-                    case 0:
-                        $result['bank_card_type'] = '无法识别';
-                        break;
-                    case 1:
-                        $result['bank_card_type'] = '借记卡';
-                        break;
-                    case 2:
-                        $result['bank_card_type'] = '信用卡';
-                        break;
-                    default:
-                        break;
-                }
-            } else {
-                $result['bank_card_type'] = '无法识别';
-            }
-
-            return $result;
-        } else {
-            return false;
-        }
     }
 
     //腾讯的文字识别接口,暂时没有使用
